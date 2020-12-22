f40bf7592320ea1c1c7bf2a842c7441ae0c9ae99
Unai <unaietx96@gmail.com>
2020-12-22 18:30:43 +0100

    Cambios en interfaz

--- EMPRESA/Controllers/HomeController.cs
+++ EMPRESA/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace EMPRESA.Controllers
 {
@@ -21,13 +25,58 @@ namespace EMPRESA.Controllers
 
         public async Task<IActionResult> Index()
         {
-            //return View();
+            ViewData["img_title"] = "Floristeria";
+            ViewData["img1_text"] = "Las mejores flores de Gipuzkoa";
+            ViewData["img2_text"] = "Descuentos en flores de interior";
+            ViewData["img3_text"] = "Abonos al 50% de descuento";
+            ViewData["img4_text"] = "Plantas para navidades";
+
             return View(await _context.Producto.ToListAsync());
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            ViewBag.titulo = "Contactos";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto(string nombre, string apellido, string correo, string comentario) //estas variables deben coincidir con el atributo name del formulario
+        {
+            enviarCorreo(nombre, apellido, correo, comentario);
+            ViewBag.titulo = "¡Gracias por contactar!";
+            return View();
+        }
+
+        public void enviarCorreo(string nombre, string apellido, string correo, string comentario)
+        {
+            string destino = "micorreo@gmail.com";
+            MailMessage msg = new MailMessage();
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = new System.Net.NetworkCredential()
+            {
+                UserName = "micorreo@gmail.com",
+                Password = "mipassword"
+            };
+
+            smtp.EnableSsl = true;
+
+            msg.From = new MailAddress(destino);
+            msg.To.Add(destino);
+            msg.Subject = "Contacto de " + nombre + " " + apellido;
+            msg.IsBodyHtml = true;
+            msg.Body = "Comentario de " + nombre + " " + apellido + "<br>" + comentario + "<br>Para responder, enviar un correo a: " + correo;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+           
+            smtp.Send(msg);
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
